c65137a3174f4e5fba7f13d678d813225311b713
Eddie Stump <ejstump13@gmail.com>
2024-06-27 16:13:28 -0400

    particle animation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,35 @@
 const canvas = <HTMLCanvasElement> document.getElementById("canvas");
 const ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
 
+type particle = {
+  x: number;
+  y: number;
+  radius: number;
+}
 
-var x = 0;
+const particles: particle[] = []
+
+ctx.fillStyle = "blue";
+
 const animationLoop = () => {
   window.requestAnimationFrame(animationLoop);
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  ctx.fillStyle = "blue";
-  ctx.fillRect(x, 100, 200, 100);
-  x += 5;
-  if (x > canvas.width) x = -200;
+  particles.push({x: Math.random() * canvas.width, y: canvas.height, radius: 70})
+
+  for (let x in particles) {
+    ctx.beginPath();
+    ctx.arc(particles[x].x, particles[x].y, particles[x].radius, 0, Math.PI*2);
+    ctx.closePath();
+    ctx.fill();
+
+    particles[x].y -= 0.5;
+    particles[x].radius -= 0.25;
+
+    if (particles[x].radius <= 0) delete particles[x]
+  }
+
+
 }
 
 window.requestAnimationFrame(animationLoop);
