1d295df06dda27785d9a5a2fee26de9f39b5dbad
Eddie Stump <ejstump13@gmail.com>
2024-06-27 15:42:04 -0400

    created html canvas

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 const canvas = <HTMLCanvasElement> document.getElementById("canvas");
 const ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
 
-ctx.fillText("Hello, world!", 10, 20);
+ctx.font = "30px Arial";
+ctx.fillText("Hello, World!", 10, 30);
