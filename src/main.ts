396dd61c46503de45f8a3afe7796069f7ae5da3f
Eddie Stump <ejstump13@gmail.com>
2024-06-28 00:39:01 -0400

    black balls scared of mouse

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ const mouse = {
   click: false,
 };
 canvas.addEventListener("mousemove", (ev) => {
-  mouse.x = ev.x;
-  mouse.y = ev.y;
+  mouse.x = ev.offsetX;
+  mouse.y = ev.offsetY;
   mouse.click = ev.buttons == 1;
 });
 
@@ -31,20 +31,30 @@ type particle = {
 const particles: particle[] = [];
 var frame = 0;
 
-ctx.fillStyle = "blue";
-
 const animationLoop = () => {
+  // Recursive
   window.requestAnimationFrame(animationLoop);
+
+  // Clear screen
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw mouse dot
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 10, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
 
   // Create new particle
   particles.push({
     x: Math.random() * canvas.width,
     y: -20,
     xVelocity: (Math.random() - 0.5) * 2,
-    yVelocity: Math.random() * 2 + 3,
+    yVelocity: (Math.random() - 0.5) * 4,
     radius: 20,
   });
+
+  ctx.fillStyle = "black";
 
   for (let p in particles) {
     // Draw particle
@@ -59,10 +69,21 @@ const animationLoop = () => {
     ctx.closePath();
     ctx.fill();
 
+    // Particles are scared of the mouse
+    if (
+      Math.sqrt(
+        Math.pow(particles[p].x - mouse.x, 2) +
+          Math.pow(particles[p].y - mouse.y, 2)
+      ) < 100
+    ) {
+      particles[p].x += (particles[p].x - mouse.x) / 30;
+      particles[p].y += (particles[p].y - mouse.y) / 30;
+    }
+
     // Update x, y, and radius
     particles[p].y += particles[p].yVelocity;
     particles[p].x += particles[p].xVelocity;
-    particles[p].radius -= 0.05;
+    particles[p].radius -= 0.02;
 
     // Loop on edges of screen, ensure no popping in and out by subtracting radius
     if (particles[p].x > canvas.width + particles[p].radius)
@@ -74,7 +95,7 @@ const animationLoop = () => {
     if (particles[p].y < -particles[p].radius)
       particles[p].y += canvas.height + particles[p].radius * 2;
 
-    // If particle is too small, delete
+    // If particle is too small, delete it
     if (particles[p].radius <= 0) delete particles[p];
 
     frame += 1;
